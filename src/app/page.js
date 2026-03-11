1c3ec7d4e4f94fe1e3a666229742c10e49b7539c
aibrahimnabil19 <aibrahimnabil@gmail.com>
2026-03-11 19:34:36 +0100

    Comment out Header component in Home page

--- src/app/page.js
+++ src/app/page.js
@@ -4,7 +4,7 @@ import Login from "@/components/Login";
 export default function Home() {
   return (
     <main className="max-w-5xl mx-auto pt-8 min-h-screen flex flex-col items-center gap-10">
-      <Header/>
+      {/* <Header/> */}
       <Login/>
     </main>
   );
